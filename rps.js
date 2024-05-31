56d8c103bd7671100ef54cd7d71b159b19487d07
srizzojr <samrizzo2@gmail.com>
2024-05-31 10:00:45 -0400

    Added choice buttons for each option and basic styling for page. Added functions for the required game states.

--- rps.js
+++ rps.js
@@ -1,1 +1,19 @@
 // JS File for Rock, Paper, Scissors Project
+let playerChoice = computerChoice = "";
+let playerScore = computerScore = 0;
+
+function playGame() {
+    alert('Game has started');
+}
+
+function evalWinCondition() {
+    
+}
+
+function setScore() {
+
+}
+
+function endGame() {
+
+}
